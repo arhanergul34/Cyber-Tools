492346bcf3ce76ae8fb63ba0da2b31544f19f994
arhanergul <arhanergul34@gmail.com>
2026-04-05 14:45:23 +0200

    Username and password verification added (and operator)

--- security_control.py
+++ security_control.py
@@ -1,17 +1,19 @@
-# We are setting a target password
+# Target Informations
+target_user = "admin"
 target_password = "cyber123"
 
 # We are collecting input from the user
-entered_password = input("Enter the password to log in to the system: ")
+entered_name = input("Enter your username: ")
+entered_password = input("Enter your password : ")
 
-# Desicion making phase
-if entered_password == target_password:
+# We check both conditions simultaneously.
+if target_user == entered_name  and entered_password == target_password:
     print ("----------------------------------")
-    print ("[+] Access Approved! Welcome to the system. ")
+    print ("[+] Login Successful! Welcome, Administrator. ")
     print ("----------------------------------")
 else:
     print ("----------------------------------")
-    print ("[-] INCORRECT PASSWORD! Access Denied. ")
+    print ("[-] ERROR: Incorrect username or password! ")
     print ("----------------------------------")
     
     
